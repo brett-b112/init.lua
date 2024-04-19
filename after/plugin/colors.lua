2ecd9cdb0792be13fd208b7ffd5d9112c2266ee3
Brett Balquist <brett@balquist.com>
2024-04-19 12:54:43 -0500

    Updated colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-	color = color or "catppuccin-mocha"
+	color = color or "catppuccin-frappe"
     -- colorscheme catppuccin " catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 	vim.cmd.colorscheme(color)
     -- This sets the cursor highlight to be an underline
--- lua/brett/remap.lua
+++ lua/brett/remap.lua
@@ -3,3 +3,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- This is suppose to execute python file with F4
+vim.keymap.set('n', '<F9>', ":w <bar> exec '!python3 '.shellescape('%')<CR>")
+
+
